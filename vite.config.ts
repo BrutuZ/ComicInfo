d3beb15c220431d571c255eab723954ac9722f7d
BrutuZ <BrutuZ@users.noreply.github.com>
2025-10-09 20:23:54 -0300

    Fix relative paths for GHPages

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { viteExternalsPlugin } from 'vite-plugin-externals'
 const isDev = process.env.NODE_ENV != 'production'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ComicInfo/',
+  base: isDev ? '' : '/ComicInfo/',
   plugins: [
     vue(),
     vueJsx(),

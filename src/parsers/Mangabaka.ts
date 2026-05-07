1e2532cb088329cfc861d60c5463f6e8974fac56
BrutuZ <BrutuZ@users.noreply.github.com>
2026-05-07 16:12:02 -0300

    Sort tags

--- src/parsers/Mangabaka.ts
+++ src/parsers/Mangabaka.ts
@@ -127,28 +127,34 @@ export function MangaBaka(url: string = '', options: ParserOptions = {}): Search
             )?.title) || page.titles.find(t => t.is_primary)?.title,
       genre: [
         ...[
-          ...(page.tags_v2 || []).filter(
-            (t, _, a) =>
-              t.name_path.startsWith('Theme') &&
-              !a.map(x => x.parent_id).includes(t.id) &&
-              t.implied_by_tag_ids.length == 0,
-          ),
+          ...(page.tags_v2 || [])
+            .filter(
+              (t, _, a) =>
+                t.name_path.startsWith('Theme') &&
+                !a.map(x => x.parent_id).includes(t.id) &&
+                t.implied_by_tag_ids.length == 0,
+            )
+            .sort(),
           ...(page.genres || [])
             .filter(g => !page.tags_v2.map(t => t.name.toLowerCase()).includes(g.toLowerCase()))
             .map(g => {
               return { name: g, name_path: `Genre > ${capitalizeTags(g)}` }
-            }),
-          ...(page.tags_v2 || []).filter(
-            (t, _, a) =>
-              !t.name_path.startsWith('Theme') &&
-              !a.map(x => x.parent_id).includes(t.id) &&
-              t.implied_by_tag_ids.length == 0,
-          ),
+            })
+            .sort(),
+          ...(page.tags_v2 || [])
+            .filter(
+              (t, _, a) =>
+                !t.name_path.startsWith('Theme') &&
+                !a.map(x => x.parent_id).includes(t.id) &&
+                t.implied_by_tag_ids.length == 0,
+            )
+            .sort(),
           ...(page.tags || [])
             .filter(t => !page.tags_v2.map(tt => tt.name.toLowerCase()).includes(t.toLowerCase()))
             .map(t => {
               return { name: t, name_path: `Other > ${t}` }
-            }),
+            })
+            .sort(),
         ].map(t =>
           options.groupTags
             ? [t.name_path.split(' > ')[0], t.name_path.split(' > ').pop()].join(':')
